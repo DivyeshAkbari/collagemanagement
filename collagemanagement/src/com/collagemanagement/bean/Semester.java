7532392f08d543027b655f3406068e353fc3f6a9
Excel <Excel@Divyesh>
2021-02-11 09:28:08 +0530

    semester bean push

---
+++ collagemanagement/src/com/collagemanagement/bean/Semester.java
@@ -0,0 +1,15 @@
+package com.collagemanagement.bean;
+
+public class Semester
+{
+		private int semid;
+
+		public int getSemid() {
+			return semid;
+		}
+
+		public void setSemid(int semid) {
+			this.semid = semid;
+		}
+		
+}

00bfc5bb499f5ec86ffbda1daaaed347318be91e
Excel <Excel@Divyesh>
2021-02-14 09:52:34 +0530

    get otp aayu

--- collagemanagement/src/com/collagemanagement/servlet/GetOTP.java
+++ collagemanagement/src/com/collagemanagement/servlet/GetOTP.java
@@ -3,7 +3,6 @@ package com.collagemanagement.servlet;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;

6c64593726b9a7162ed5f182c81a8ebd38b981fa
Excel <Excel@Divyesh>
2021-04-18 16:01:12 +0530

    done

--- collagemanagement/src/com/collagemanagement/bean/Event.java
+++ collagemanagement/src/com/collagemanagement/bean/Event.java
@@ -8,7 +8,12 @@ public class Event {
 	private String eventDescription;
 	private String userProfilepic;
 	private InputStream userProfilepicStream;
-	
+	private int eventid;
+	private String date;
+	private int isactive;
+	private int d1;
+	private String month;
+	private int year;
 	@Override
 	public String toString() {
 		return String.format("Event [eventname=%s, eventDescription=%s, userProfilepic=%s, userProfilepicStream=%s]",
@@ -38,5 +43,53 @@ public class Event {
 	}
 	public void setUserProfilepicStream(InputStream userProfilepicStream) {
 		this.userProfilepicStream = userProfilepicStream;
+	}
+
+	public int getEventid() {
+		return eventid;
+	}
+
+	public void setEventid(int eventid) {
+		this.eventid = eventid;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getIsactive() {
+		return isactive;
+	}
+
+	public void setIsactive(int isactive) {
+		this.isactive = isactive;
+	}
+
+	public int getD1() {
+		return d1;
+	}
+
+	public void setD1(int d1) {
+		this.d1 = d1;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
 	}
 }
---
+++ collagemanagement/src/com/collagemanagement/bean/Feedback.java
@@ -0,0 +1,61 @@
+package com.collagemanagement.bean;
+
+public class Feedback
+{
+	private int userid;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getRoll() {
+		return roll;
+	}
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	private String description;
+	private String date;
+	private String roll;
+	private String subject;
+	private String email;
+	private String name;
+	private String contact;
+}

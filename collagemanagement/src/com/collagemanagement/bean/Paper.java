37a7f5567d09c7354c1d929df30f6e2376b5857d
Excel <Excel@Divyesh>
2021-05-08 09:59:27 +0530

    paper bean

--- collagemanagement/src/com/collagemanagement/bean/Paper.java
+++ collagemanagement/src/com/collagemanagement/bean/Paper.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 public class Paper
 {
 	private int Streamid;
+	private String streamname;
+	private int paperid;
+	private String papertype;
 	public int getStreamid() {
 		return Streamid;
 	}
@@ -42,10 +45,12 @@ public class Paper
 		PDFString = pDFString;
 	}
 	
+	
 	@Override
 	public String toString() {
-		return "Paper [Streamid=" + Streamid + ", Semesterid=" + Semesterid + ", Subjectid=" + Subjectid + ", Year="
-				+ Year + ", PDF=" + PDF + ", PDFString=" + PDFString + ", Type=" + Type + "]";
+		return "Paper [Streamid=" + Streamid + ", streamname=" + streamname + ", Semesterid=" + Semesterid
+				+ ", Subjectid=" + Subjectid + ", Year=" + Year + ", PDF=" + PDF + ", PDFString=" + PDFString
+				+ ", Type=" + Type + "]";
 	}
 	public String getType() {
 		return Type;
@@ -53,10 +58,29 @@ public class Paper
 	public void setType(String type) {
 		Type = type;
 	}
+	public String getStreamname() {
+		return streamname;
+	}
+	public void setStreamname(String streamname) {
+		this.streamname = streamname;
+	}
+	public int getPaperid() {
+		return paperid;
+	}
+	public void setPaperid(int paperid) {
+		this.paperid = paperid;
+	}
+	public String getPapertype() {
+		return papertype;
+	}
+	public void setPapertype(String papertype) {
+		this.papertype = papertype;
+	}
 	private int Semesterid;
 	private int Subjectid;
 	private int Year;
 	private InputStream PDF;
 	private String PDFString;
 	private String Type;
+	 
 }
